d8902fee4268a8e87c3702c5af9c329c06c3b6fd
Abhishek Kumar <sagargraphics2020@gmail.com>
2026-02-11 00:06:06 +0530

    Initial project commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignupSerializer
+
+class SignupView(APIView):
+    def post(self,request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"User created"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
