88396ea93ae44d23b0eaac42abc44b8b9190ce8f
Abhishek Kumar <sagargraphics2020@gmail.com>
2026-02-12 23:31:09 +0530

    Initial project commit

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -122,3 +122,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'users.User'
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+}
--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
+     path('users/login/', TokenObtainPairView.as_view()),
 ]
