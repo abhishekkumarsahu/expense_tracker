879eb1fddda4251ab98dd1c8ded4bd4ea59d26d2
Abhishek Kumar <sagargraphics2020@gmail.com>
2026-02-12 23:47:22 +0530

    Initial project commit

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('Groceries','Groceries'),
+        ('Leisure', 'Leisure'),
+        ('Electronics', 'Electronics'),
+        ('Utilities', 'Utilities'),
+        ('Clothing', 'Clothing'),
+        ('Health', 'Health'),
+        ('Others', 'Others'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
